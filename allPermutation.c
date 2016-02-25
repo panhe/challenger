1c3e8b7c558262cd21c1b400af73bd21a59ebaf6
panhe <panhe_sy@126.com>
2016-02-25 16:05:14 +0800

    Create allPermutation.c

---
+++ allPermutation.c
@@ -0,0 +1,44 @@
+/*
+  字符串的所有排列，举个例子，输入字符串“ab”，所有的排列为“aa”，“ab”，“ba”，“bb”
+*/
+#include "stdio.h"
+#include "string.h"
+#include "malloc.h"
+int getPermutation(char *s, char *tmp, int len, int size);
+
+int main()
+{
+	char *s="abc";
+	int size=strlen(s);
+	int count;
+	char *tmp=(char *)malloc(sizeof(char)*size);//分配空间
+	memset(tmp,0,sizeof(char)*size);//初始化
+	count=getPermutation(s,tmp,0,size);
+	printf("total num is %d\n",count);//输出排列的个数
+	return 0;
+}
+
+int getPermutation(char *s, char *tmp,int len,int size)
+{
+	if(s==NULL) return 0;
+	int i,j;
+	//static的特性，保存值
+	static count=0;
+	//设置一个长度，达到字符串总长度时就打印
+	if(len<size)
+	{
+		for(i=0;i<size;i++)
+		{
+			tmp[len]=s[i];
+			getPermutation(s,tmp,len+1,size);
+		}
+	}
+	else
+	{
+		count++;
+		for(j=0;j<size;j++)
+			printf("%c",tmp[j]);
+		printf("\n");
+	}
+	return count;
+}
